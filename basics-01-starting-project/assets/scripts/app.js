dda2cab8ac97b9deb06c115d8bdd085b04910ea6
_yoonyoon_1 <heeyoon1302@naver.com>
2023-02-22 21:08:10 +0900

    색션 2,3 정리

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,13 +1,56 @@
 let defaultAnswer = 0;
 let currentAnswer = defaultAnswer;
-currentAnswer = (currentAnswer + 10) * 3 / 2 -1;
+let logEntries = [];
 
-function add (a, b){
-    result = a+b;
-    alert(result);
+function getUserInputNumber(){
+    return parseInt(userInput.value);
 }
 
-add(1,2);
+function writeToLog(prevValue, inputValue,operator, result ){
+    const entry = {
+        prevValue : prevValue,
+        inputValue : inputValue,
+        operator : operator,
+        result : result
+    }
+    logEntries.push(entry);
+    console.log(logEntries);
+}
 
-let calculationDescription = `(${currentAnswer} + 10) * 3 / 2 -1`;
-outputResult(currentAnswer, calculationDescription);
+function outputResultDescription(currentAnswer, operator, inputNumber){
+    return `${currentAnswer} ${operator} ${inputNumber}`
+}
+
+function add (){
+    const enteredValue = getUserInputNumber();
+    const initialValue = currentAnswer;
+    currentAnswer += enteredValue;
+    outputResult(currentAnswer, outputResultDescription(initialValue, '+', getUserInputNumber()))
+    writeToLog(initialValue, enteredValue, 'ADD', currentAnswer);
+}
+function subtract (){
+    const enteredValue = getUserInputNumber();
+    const initialValue = currentAnswer;
+    currentAnswer = currentAnswer - getUserInputNumber();
+    outputResult(currentAnswer, outputResultDescription(initialValue, '-', getUserInputNumber()))
+    writeToLog(initialValue,enteredValue, 'SUB', currentAnswer);
+}
+function multiply (){
+    const enteredValue = getUserInputNumber();
+    const initialValue = currentAnswer;
+    currentAnswer = currentAnswer * getUserInputNumber();
+    outputResult(currentAnswer, outputResultDescription(initialValue, '*', getUserInputNumber()))
+    writeToLog(initialValue,enteredValue, 'MUL', currentAnswer);
+}
+function divide (){
+    const enteredValue = getUserInputNumber();
+    const initialValue = currentAnswer;
+    currentAnswer = currentAnswer / getUserInputNumber();
+    outputResult(currentAnswer, outputResultDescription(initialValue, '/', getUserInputNumber()))
+    writeToLog(initialValue,enteredValue, 'DIV', currentAnswer);
+}
+addBtn.addEventListener('click',add)
+subtractBtn.addEventListener('click',subtract)
+multiplyBtn.addEventListener('click',multiply)
+divideBtn.addEventListener('click',divide)
+
